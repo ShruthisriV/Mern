12cfe2b30e1e0dba3171450d2845a2d64406eb7e
ShruthisriV <shruthiraoveerla06@gmail.com>
2025-02-21 09:20:56 +0530

    path params, Query params

--- Intro/express/index.js
+++ Intro/express/index.js
@@ -4,12 +4,39 @@ const express = require("express");
 //create an express application
 const app = express();
 
-app.get("/", (req,res) => {
-    res.send("Hello from Express");  //res.write() + res.end()
+const users = [
+    { id: 1, name: "user1"},
+    { id: 2, name: "user2"}
+];
+
+app.get("/users", (req,res) => {
+    // res.send("Hello from Express");  //res.write() + res.end()
+    let queryparams = req.query;
+    
+    console.log(queryparams);
+    //on the basis of query sent, we can query the db and get results
+    res.status(200).json({
+        message: "All Users",
+        userList: users
+    })
 });
 
-app.get("/trendingsongs", (req, res) => {
-    res.send("List of trending Songs");
+//query params or route params
+app.get("/users/:id", (req, res) => {
+    let {id: userId} = req.params;
+    console.log(typeof userId, userId);
+    const user = users.find(user => user.id == userId);
+    if(user){
+        res.status(200).json({
+            message: "User found",
+            user: user
+        })
+    }else{
+        res.status(200).json({
+            message: "User not found",
+            user: []
+        })
+    }
 });
 
 const port = 3000;
