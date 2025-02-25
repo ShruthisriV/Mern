2d8e2fbef99339bfd823615b6eea3396e9ba38d5
ShruthisriV <shruthiraoveerla06@gmail.com>
2025-02-25 10:32:14 +0530

    login and register page

--- bookMyShow-project/client/src/App.jsx
+++ bookMyShow-project/client/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
           <Route path='/register' element={<Register/>}/>
           <Route path='/login' element={<Login/>}/>
         </Routes>
-      </BrowserRouter>
+        </BrowserRouter>
     </div>
   );
 }
