917a29dcd2a71e69156e55cf27361b14b07c4c7b
ShruthisriV <shruthiraoveerla06@gmail.com>
2025-03-12 08:52:02 +0530

    forget, reset, email otp, email ticket

--- bookMyShow-project/server/utils/emailHelper.js
+++ bookMyShow-project/server/utils/emailHelper.js
@@ -1,21 +1,39 @@
 const nodemailer = require("nodemailer");
+const path = require("path");
+const fs = require("fs");
+const dotenv = require("dotenv").config({ path: "../.env" });
+const { SENDGRID_API_KEY } = process.env;
+
+function replaceContent(content, creds) {
+  let allKeys = Object.keys(creds); //[name,otp];
+  allKeys.forEach((key) => {
+    content = content.replace(`##${key}`, creds[key]);
+  });
+  return content;
+}
 async function emailHelper(templateName, receiverEmail, creds) {
   try {
+    const templatePath = path.join(
+      __dirname,
+      "email_templates",
+      templateName + ".html"
+    );
+    let content = await fs.promises.readFile(templatePath, "utf-8");
     const transportDetails = {
       host: "smtp.sendgrid.net",
       port: 587,
       auth: {
-        user: "",
-        pass: "",
+        user: "apikey",
+        pass: SENDGRID_API_KEY,
       },
     };
 
     const emailDetails = {
-      from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>', // sender address
-      to: "bar@example.com, baz@example.com", // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
-      html: "<b>Hello world?</b>", // html body
+      from: "shruthiraoveerla06@gmail.com", // sender address
+      to: receiverEmail, // list of receivers
+      subject: "Mail from developer", // Subject line
+      text: `Hi ${creds.name} this is your reset otp ${creds.otp}`, // plain text body
+      html: replaceContent(content, creds), // html body
     };
     const transporter = nodemailer.createTransport(transportDetails);
     // send mail with defined transport object
@@ -24,5 +42,10 @@ async function emailHelper(templateName, receiverEmail, creds) {
     console.log(err);
   }
 }
+
+emailHelper("otp", "shruthiraoveerla06@gmail.com", {
+  name: "Krishan",
+  otp: "1234",
+});
 
 module.exports = emailHelper;
